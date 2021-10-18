10ea05863edaad56ffc4087df44dbf0153a24479
Zvrlry <cduggan600@gmail.com>
2021-10-18 09:05:10 -0400

    Inventory Item Equips

--- Nexus/Assets/Scripts/Item.cs
+++ Nexus/Assets/Scripts/Item.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 
 public class Item : ScriptableObject
 {
-	new public string name = "new item";
-	public Sprite icon = null;
-	public bool isDefaultItem = false;
+	new public string name = "new item"; //name of the item
+	public Sprite icon = null;			//Item Icon
+	public bool isDefaultItem = false;	//Is the item default wear? 
 
-
+	//called when the item i pressed in the inventory
 	public virtual void Use()
 	{
+		
 		Debug.Log("Using " + name);
 	}
 
+	public void RemoveFromInventory()
+	{
+		Inventory.instance.Remove(this);
+	}
 }
